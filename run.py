25f163ca908e430c6ebbe3c013910601de59a4e8
MSFTserver <sawyerlemmo@gmail.com>
2022-02-12 23:24:12 -0800

    fix run.py with slight improvement

--- run.py
+++ run.py
@@ -37,4 +37,4 @@ out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
 
 out_img = Image.merge('YCbCr', [out_img_y, cb, cr]).convert('RGB')  # we merge the output of our network with the upscaled Cb and Cr from before
                                                                     # before converting the result in RGB
-out_img.save(f"{args.image}_{suffix}")
+out_img.save(f"{name}_{suffix}")
